4a17ab95d0dffa308ba848905387babea3a2913d
colincove <cove.colin@gmail.com>
2015-07-13 21:38:58 -0700

    camera controls restored to chartacter.

---
+++ Profundum/Assets/CameraAnchorControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAnchorControl : MonoBehaviour {
+	public GameObject target;
+	public float radius = 100;
+	public float radiusWindow = 0.5f;
+	public int pitchCeiling = 45;
+	public int pitchFloor = -20;
+	public float damp = 20;
+
+	private float _pitch;
+	private float _yaw;
+	private float _r;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+
+		float pitchDelta =  Mathf.DeltaAngle(pitchFloor, _pitch / (Mathf.PI/180));
+		float pitchWindow =  Mathf.DeltaAngle(pitchFloor, pitchCeiling);
+		_r = radius - ((radius * radiusWindow)) * (1 - pitchDelta / pitchWindow);
+		
+		float p = _pitch;
+		p = (-1 + ((pitchDelta / pitchWindow) * (pitchCeiling + 1))) * (Mathf.PI/180);
+		
+		Vector3 pos = new Vector3(
+			(float)(target.transform.position.x + _r * Mathf.Cos(p) * Mathf.Cos(_yaw)), 
+			(float)(target.transform.position.y + .25 + _r * Mathf.Sin(p)),
+			(float)(target.transform.position.z+ _r *Mathf.Cos(p) * Mathf.Sin(_yaw)));
+		transform.position = pos;
+		_pitch += Input.GetAxis("JoystickRightVertical") / damp;
+		_yaw -= Input.GetAxis("JoystickRightHorizontal") / damp;
+		
+		if(_pitch > pitchCeiling *(Mathf.PI/180))
+		{
+			_pitch = pitchCeiling *(Mathf.PI/180);
+		}
+		else if(_pitch < pitchFloor *(Mathf.PI/180))
+		{
+			_pitch = pitchFloor *(Mathf.PI/180);
+		}
+	}
+}
---
+++ Profundum/Assets/scripts/MainCameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraMovement : MonoBehaviour {
+	public GameObject target;
+	public float speed = 1f;
+	public float damp = 10;
+	// Use this for initialization
+	void Awake () {
+	
+	}
+
+	void Start () {
+		if (target) {
+			transform.position = target.transform.position;
+			transform.rotation = target.transform.rotation;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			float step = Vector3.Distance(transform.position, target.transform.position) / damp;
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+			
+			step = Quaternion.Angle(transform.rotation, target.transform.rotation) / damp;
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, step);
+		}
+	}
+	void ChangeTarget(GameObject newTarget)
+	{
+		target = newTarget;
+	}
+}

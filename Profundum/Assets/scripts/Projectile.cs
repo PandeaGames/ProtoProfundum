a0243102239be904047c860bde8c13a83c3407cc
colincove <cove.colin@gmail.com>
2015-07-25 16:20:45 -0700

    First set of in game UI. Dot in middle of screen. Lights from light shots fade out instead of snapping off.

--- Profundum/Assets/scripts/Projectile.cs
+++ Profundum/Assets/scripts/Projectile.cs
@@ -5,14 +5,14 @@ public class Projectile : MonoBehaviour {
 	public float speed = 1.0f;
 	public float lifeTime = 3;
 
-	private float _startTime;
+	protected float _startTime;
 	// Use this for initialization
-	void Start () {
+	public virtual void Start () {
 		_startTime = Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public virtual void Update () {
 		transform.Translate (Vector3.forward * speed);
 		//transform.position = transform.position. + (Vector3.forward * speed);
 		if (Time.time - _startTime > lifeTime) {

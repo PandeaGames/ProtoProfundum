b2396aa1ce01e8900122b9b07592ccfcda6fc361
Airemon <airemon12@gmail.com>
2015-09-23 00:19:23 -0700

    #audio - Tuned ambience & implemented it in game, created footsteps and tied them to the event Play_Footstep, created a new script (AkFootsteps) for use to control footsteps...no idea how to script that shit..

---
+++ Profundum/Assets/AkFootsteps.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AkFootsteps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

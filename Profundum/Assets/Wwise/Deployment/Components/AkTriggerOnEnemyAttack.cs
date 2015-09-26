758f6ad7ebccdf7abde7556fc93ea37e42b83d21
Airemon <airemon12@gmail.com>
2015-09-26 11:57:06 -0700

    #audio - Created sounds for floating enemy attacks, added assets to Wwise and created an Enemy_Attack Wwise event.

---
+++ Profundum/Assets/Wwise/Deployment/Components/AkTriggerOnEnemyAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+public class TriggerOnEnemyAttack : AkTriggerBase
+{
+	void Attacking()
+	{
+		if(triggerDelegate != null)
+		{
+			triggerDelegate(null);
+		}
+	}
+}
